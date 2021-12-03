02b1ad850e5dd7dd01635e95288426f814781eed
David Ildefonso <david_lion_ig@outlook.com.pe>
2021-12-02 19:03:48 -0500

    end of part 3c

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
+
+require('dotenv').config();
+
+
+const Note = require('./models/note');
+
+
+
 const app = express();
 
 app.use(express.json({strict: false}));
@@ -9,26 +17,29 @@ app.use(cors());
 app.use(express.static('build'));
 
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-];
+
+
+
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2019-05-30T17:30:31.098Z",
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2019-05-30T18:39:34.091Z",
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2019-05-30T19:20:14.298Z",
+//     important: true
+//   }
+// ];
 
 
 app.get("/", (request, response ) => {
@@ -37,35 +48,58 @@ app.get("/", (request, response ) => {
 
 app.get("/api/notes", (request, response) => {
 	
-		response.json(notes);
+		
+		Note.find({}).then(notes => {
+				response.json(notes);
+			
+		})
 });
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id;
-  const note = notes.find(note => note.id === parseInt(id));
+ 
 
-  if(note){
-			response.json(note);
-	}else{
-			response.status(404).end("Note not found");
-	}
+	Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
+
 
 });
 
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
-	response.status(204).end();
+
+		Note.findById(request.params.id)
+			.then(note => {
+						if(!note){
+								return response.status(400).json({ 
+									error: 'note not found' 
+								});
+						}				
+
+						Note.findByIdAndRemove(request.params.id , function (err, note) {
+							if (err) return console.log(err);
+							response.status(204).end();
+						});
+						
+			}).catch(error => {
+					console.log(error)
+					return response.status(400).json({ 
+						error: 'an error ocurred' 
+					});
+			})
+
+
+
+
 });
 
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+// const generateId = () => {
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => n.id))
+//     : 0
+//   return maxId + 1
+// }
 
 
 
@@ -78,41 +112,60 @@ app.post('/api/notes', (request, response) => {
 			});
 		}
 
-		const note = {
+		const newNoteContent = {
 			content: body.content,
 			important: body.important || false,
-			date: new Date(),
-			id: generateId(),
-		}
+			date: new Date()
+		};
 
-		notes = notes.concat(note);
-  	response.json(note);
+		const note = new Note(newNoteContent);
+
+		note.save().then(savedNote => {
+			response.json(savedNote)
+		});
+
 })
 
 
 
 app.put('/api/notes/:id', (request, response) => {
 
-		const id = Number(request.params.id);
- 		const note = notes.find(note => note.id === id);
-		
-		if(!note){
-			return response.status(400).json({ 
-				error: 'note not found' 
-			});
-		}
 
-		const updatednote = { ... note, important : !note.important };
 
-		notes = notes.map(n => n.id === id ? updatednote : n);
+		Note.findById(request.params.id)
+			.then(note => {
+						if(!note){
+								return response.status(400).json({ 
+									error: 'note not found' 
+								});
+						}				
 
-  	response.json( notes.find(note => note.id === id));
+						const important = !note.important ;
+			
+						Note.findByIdAndUpdate(
+								request.params.id,
+								{ $set:	{	"important": important} },
+								{new: true},
+								function (err, note) {
+									if (err) return console.log(err);
+									response.json(note)
+						})
+
+			}).catch(error => {
+					console.log(error)
+					return response.status(400).json({ 
+						error: 'an error ocurred' 
+					});
+			})
+
+	
+
 })
 
 
 
 
-const PORT  =  process.env.PORT || 3001;
+const PORT  =  process.env.PORT;
 
 app.listen(PORT, () => {
 		console.log(`Server running on port ${PORT}`);
